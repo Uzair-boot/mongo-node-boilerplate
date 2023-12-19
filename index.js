0c924270e3a9b681fb79222dff986f1c0b2bec9a
uzair-boot <ozairahmad984@gmail.com>
2023-12-20 01:47:54 +0500

    reverse sale date wth slashes

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const express = require ('express')
+const express = require("express");
 const app = express();
+const cors = require("cors");
+
 // const MONGO_URL = "mongodb+srv://uzair:BCGoKwvfyFUSIn3L@cluster0.zrjubhl.mongodb.net/?retryWrites=true&w=majority"
-const MONGO_URL = "mongodb+srv://aminaaslam985:amina123@cluster0.w7bvzq1.mongodb.net/?w=majority"
+// const MONGO_URL = "mongodb+srv://aminaaslam985:amina123@cluster0.w7bvzq1.mongodb.net/?w=majority"
+const MONGO_URL = "mongodb://103.18.20.49:3029/";
 
-
-
-const mongoose = require ('mongoose');
-const dotenv = require ('dotenv');
-const helmet = require ('helmet');
-const morgan = require ('morgan');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 mongoose.set("strictQuery", false);
-mongoose.connect(MONGO_URL, (err)=>{
-    if(err){
-        console.log("Unable to Connect" , err);
-    } else{
-        console.log("Connected to MongoDB");
-    }
-})
+mongoose.connect(MONGO_URL, (err) => {
+  if (err) {
+    console.log("Unable to Connect", err);
+  } else {
+    console.log("Connected to MongoDB");
+  }
+});
 
-const userRoute = require('./routes/users');
-const authRoute = require('./routes/auth');
-const postsRoute = require('./routes/posts');
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
 //middlewares
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(cors());
 
-app.use('/api/users' , userRoute);
-app.use('/api/auth' , authRoute);
-app.use('/api/posts' , postsRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 
 dotenv.config();
 
-app.listen(8800, ()=>{
-    console.log("Backend Server is running");
-})
+app.listen(8800, () => {
+  console.log("Backend Server is running");
+});
